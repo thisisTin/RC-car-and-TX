7d72bc2517b6b7a87b7fa249a23a6ec2e3249ad5
thisisTin <trantin.1589@gmail.com>
2024-10-21 14:42:14 +0700

    50%

--- Radio Control (Tx)/RadioControl_Transmitter/RadioControl_Transmitter.ino
+++ Radio Control (Tx)/RadioControl_Transmitter/RadioControl_Transmitter.ino
@@ -1,16 +1,19 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-
+Adafruit_SSD1306 display;
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+int progress=0; 
 const uint64_t pipeOut = 0xE9E8F0F0E1LL;   //IMPORTANT: The same as in the receiver 0xE9E8F0F0E1LL 
 RF24 radio(7, 8); // select CE,CSN pin | 
 
 struct Signal {
 byte throttle;
-byte pitch;
-byte roll;
-byte yaw;
+byte steer;
 };
 
 Signal data;
@@ -18,42 +21,75 @@ Signal data;
 void ResetData() 
 {
 data.throttle = 127; // Motor Stop (254/2=127)| 
-data.pitch = 127; // Center 
-data.roll = 127; // Center 
-data.yaw = 127; // Center 
+data.steer = 127; // Center 
 }
 
 void setup()
 {
 //Start everything up
-
+Serial.begin(9600);
 radio.begin();
 radio.openWritingPipe(pipeOut);
 radio.stopListening(); //start the radio comunication for Transmitter | 
 ResetData();
-}
-
-// Joystick center and its borders | 
-
-int mapJoystickValues(int val, int lower, int middle, int upper, bool reverse)
+//OLED   
+display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // Initialize display with the I2C address of 0x3C
+display.clearDisplay();  // Clear the buffer
+display.setTextColor(WHITE);  // Set color of the text
+display.setRotation(0);  // Set orientation. Goes from 0, 1, 2 or 3
+display.setTextWrap(false);  // By default, long lines of text are set to automatically “wrap” back to the leftmost column.
+                               // To override this behavior (so text will run off the right side of the display - useful for
+                               // scrolling marquee effects), use setTextWrap(false). The normal wrapping behavior is restored
+                               // with setTextWrap(true).
+display.dim(0);  //Set brightness (0 is maximun and 1 is a little dim)
+ 
+display.clearDisplay();
+// LOADING PROCEDURE
+display.setTextSize(0);
+display.setTextColor(WHITE);
+display.setCursor(31, 12);
+display.print("TX Loading..");
+for (progress=0;progress<=127;progress++)
 {
-val = constrain(val, lower, upper);
-if ( val < middle )
-val = map(val, lower, middle, 0, 128);
-else
-val = map(val, middle, upper, 128, 255);
-return ( reverse ? 255 - val : val );
+  progress=progress+4;
+  display.drawRect(0, 127, 78, 6, WHITE);
+  display.setCursor(0, 17);
+  display.fillRect(1, 25, progress, 5, WHITE);
+  display.display();
+  if(progress==127)
+  {
+    loop();
+  }
+}
+display.display();
 }
 
 void loop()
 {
 // Control Stick Calibration 
 // Setting may be required for the correct values of the control levers. 
-
-data.throttle = mapJoystickValues( analogRead(A0), 524, 524, 1015, true );
-data.roll = mapJoystickValues( analogRead(A1), 12, 524, 1020, true );      // "true" or "false" for servo direction 
-data.pitch = mapJoystickValues( analogRead(A2), 12, 524, 1020, true );     // "true" or "false" for servo direction 
-data.yaw = mapJoystickValues( analogRead(A3), 12, 524, 1020, true );       // "true" or "false" for servo direction 
+//UI
+display.clearDisplay();
+display.setTextSize(0);
+//TX PROFILE
+display.setCursor(30, 0);  // (x,y)
+display.println(" RC car TX");
+data.throttle = map( analogRead(A0), 0, 1023, 0, 255 );
+data.steer = map( analogRead(A1), 0, 1023, 0, 255 );     
 
 radio.write(&data, sizeof(Signal));
+
+//THROTTLE UI
+display.setCursor(0, 10);  // (x,y)
+display.println("Throttle: ");  // Text or value to print
+display.drawRect(50, 12, 78, 4, WHITE);
+display.fillRect(51, 13, data.throttle/3.282051282, 2, WHITE);
+//Steering UI 
+display.setCursor(0, 20);  // (x,y)
+display.println("Steer: ");  // Text or value to print
+display.drawRect(50, 21, 78, 4, WHITE);
+display.fillRect(51, 22, data.steer/3.282051282, 2, WHITE); 
+//PRINT ALL
+display.display();
+
 }
--- Xe RC (Rx)/RC-cars/RC-cars.ino
+++ Xe RC (Rx)/RC-cars/RC-cars.ino
@@ -1,9 +1,73 @@
-void setup() {
-  // put your setup code here, to run once:
+//  4 Channel Receiver | 4 Kanal Alıcı
+//  PWM output on pins D2, D3, D4, D5 (Çıkış pinleri)
 
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+
+struct Signal {
+byte throttle;      
+byte pitch;
+byte roll;
+byte yaw;
+};
+
+Signal data;
+
+const uint64_t pipeIn = 0xE9E8F0F0E1LL;
+RF24 radio(7, 8); 
+
+void ResetData()
+{
+// Define the inicial value of each data input. | Veri girişlerinin başlangıç değerleri
+// The middle position for Potenciometers. (254/2=127) | Potansiyometreler için orta konum
+data.throttle = 127; // Motor Stop | 
+data.steer = 127;  // Center | 
 }
-//huhu
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void setup()
+{
+  //Set the pins for each PWM signal | Her bir PWM sinyal için pinler belirleniyor.
+  ch1.attach(2);
+  ch2.attach(3);
+  ch3.attach(4);
+  ch4.attach(5);
+
+  //Configure the NRF24 module
+  ResetData();
+  radio.begin();
+  radio.openReadingPipe(1,pipeIn);
+  
+  radio.startListening(); //start the radio comunication for receiver | Alıcı olarak sinyal iletişimi başlatılıyor
+}
+
+unsigned long lastRecvTime = 0;
+
+void recvData()
+{
+while (radio.available() ) {
+radio.read(&data, sizeof(Signal));
+lastRecvTime = millis();   // receive the data |
+}
+}
+
+void loop()
+{
+recvData();
+unsigned long now = millis();
+if ( now - lastRecvTime > 1000 ) {
+ResetData(); // Signal lost.. Reset data |
+}
+
+ch_width_1 = map(data.throttle, 0, 255, 1000, 2000);     // pin D2 (PWM signal)
+ch_width_2 = map(data.pitch,    0, 255, 1000, 2000);     // pin D3 (PWM signal)
+ch_width_3 = map(data.roll,     0, 255, 1000, 2000);     // pin D4 (PWM signal)
+ch_width_4 = map(data.yaw,      0, 255, 1000, 2000);     // pin D5 (PWM signal)
+
+// Write the PWM signal 
+ch1.writeMicroseconds(ch_width_1);
+ch2.writeMicroseconds(ch_width_2);
+ch3.writeMicroseconds(ch_width_3);
+ch4.writeMicroseconds(ch_width_4);
 }
